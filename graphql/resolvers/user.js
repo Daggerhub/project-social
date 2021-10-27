65d819dd994449ae582c9ec87a7783068a67b72b
Vishwash Bajpai <vishwash.bajpai87@gmail.com>
2021-10-27 16:45:42 +0530

    Added login mutation

--- graphql/resolvers/user.js
+++ graphql/resolvers/user.js
@@ -3,10 +3,56 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../../config');
 const { UserInputError } = require('apollo-server');
+const { validateRegisterInput, validateLoginInput } = require('../../util/validate');
+
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username
+    },
+    JWT_SECRET,
+    { expiresIn: '1h' }
+  );
+}
 
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = 'User not found';
+        throw new UserInputError('User not found', { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = 'Wrong crendetials';
+        throw new UserInputError('Wrong crendetials', { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token
+      };
+    },
+
     async register(_, { registerInput: { username, email, password, confirmPassword } }) {
+      const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword);
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
+      }
       const user = await User.findOne({ username });
 
       if (user) {
@@ -28,15 +74,7 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res._id,
-          email: res.email,
-          username: res.username
-        },
-        JWT_SECRET,
-        { expiresIn: '1h' }
-      );
+      const token = generateToken(res);
 
       return {
         ...res._doc,
